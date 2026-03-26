b23a4b44b144d0bda100bdd602ceef9a544d3b53
adrianznur <160188675+adrianznur@users.noreply.github.com>
2026-03-26 19:40:08 -0400

    Update lambda_function1.py

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -17,7 +17,7 @@ def lambda_handler(event, context=None):
         }
     
     fahrenheit = celsius * 9/5 + 32
-    fahrenheit = int(fahrenheit)
+    fahrenheit = float(fahrenheit)
     
     return {
         'statusCode': 200,
